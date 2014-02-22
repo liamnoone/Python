de626ae34f8fddb19b9c6b841978feb7a24d7030
Liam Noone <liamnoone92@gmail.com>
2014-02-22 17:28:33 +0000

    Correctly handles season-0 (special) episodes. Correctly replace illegal characters

---
+++ Anime-hash-checker.py
@@ -0,0 +1,35 @@
+import re
+import os
+from CRC import CRC
+
+
+def Process(folder):
+    total = 0
+    if (os.path.exists(folder)):
+        for file in os.listdir(folder):
+            hash = ""
+            try:
+                providedHash = re.search("\[([A-Z0-9]{8})\]", file).group(1)
+            except AttributeError:
+                continue
+            if providedHash is None:
+                continue
+
+            else:
+                total += 1
+                print(file, providedHash)
+                # Calculate hash
+                hash = CRC(os.path.join(folder, file))
+                if hash == providedHash:
+                    print("File name matches CRC32 hash:", file)
+                else:
+                    print("Invalid file: ..." +
+                          file[-30:] + ". Calclated hash is", providedHash)
+        print("\nFinished:", total, "files processed.")
+
+    else:
+        print("Directory does not exist")
+
+
+if __name__ == "__main__":
+    Process("D:/test")
--- TVDB.py
+++ TVDB.py
@@ -10,18 +10,18 @@ from bs4 import BeautifulSoup
 # TVDb API Key
 API_KEY = "C2AA47AACAA7C7B2"
 # The file types the script will work on
-FILE_TYPES = ["AVI", "MP4", "MKV"]
+FILE_TYPES = ["AVI", "MP4", "MKV", "SRT"]
 # Characters which aren't allowed in file names
-ILLEGAL_CHARACTERS = ["/", "\\", ":", "*", "?", "\"", "<", ">", "|"]
+ILLEGAL_CHARACTERS = ["/", "\\", ":", "*", "?", "\"", "<", ">", "|", "/"]
 # Character to replace those characters with
-REPLACE_CHAR = "-"
+REPLACE_CHAR = "_"
 # Pad out the season/episode with zeros to desired length
 SEASON_PADDED_ZEROS = 1
 EPISODE_PADDED_ZEROS = 2
 # What to filter out of the file name, the goal being to leave only the TV Show, Season # and Episode #
 FILTER = r"\.|-|_|\b(S?(\d+)+?x?E?(\d+)+?)\b|(\(.+\))|(\[.+\])|(\-.+\-)|\s-.+\s*|\b(\d{4})\b| \
-         480p|480i|720p|720i|1080p|1080i|HDTV|H\.264|x264|XviD|BluRay|MMI|WEB-DL|DD5\.1|AAC2\.0|DTS| \
-         IMMERSE|EVOLVE|YIFY|PublicHD|CTU|RED|DIMENSION|AFG"
+         480p|480i|720p|720i|1080p|1080i|HDTV|H\.264|x264|XviD|BluRay|MMI|WEB-DL|DD5\.1|AAC2\.0|DTS|INTERNAL|REPACK|PROPER|ReEnc| \
+         IMMERSE|EVOLVE|YIFY|PublicHD|CTU|RED|DIMENSION|AFG|MIKY|KILLERS|IMMERSE|DeeJayAhmed"
 # The language to return episode data from TVDb for. English is the only tested language
 LANGUAGE = "en" 
 
@@ -42,8 +42,8 @@ class TVShow:
     def process(self):
         release = re.findall("S?(\d+)x?E?(\d+)", self.tvshow, flags=2)
         if len(release) > 0:
-            self.season = release[0][0].lstrip("0")
-            self.episode = release[0][1].lstrip("0")
+            self.season = release[0][0].lstrip("0") if int(release[0][0]) != 0 else release[0][0]
+            self.episode = release[0][1].lstrip("0") if int(release[0][1]) != 0 else release[0][1]
 
         # Replace any fluff from the file name
         self.tvshow = re.sub(FILTER, " ", self.tvshow, flags=2).strip()
@@ -57,17 +57,17 @@ class TVShow:
 
     def get_episode(self):
         r = requests.get("http://thetvdb.com/api/{0}/series/{1}/default/{2}/{3}/{4}.xml"
-            .format(API_KEY, self.series, str(self.season).lstrip("0"), str(self.episode).lstrip("0"), LANGUAGE))
+            .format(API_KEY, self.series, str(self.season), str(self.episode), LANGUAGE))
 
         if r.status_code == 404: print("Error! 404: Not found")
         else: self.title = BeautifulSoup(r.content).find("episodename").text
 
     def replace_illegal_characters(self):
         for illegal_character in ILLEGAL_CHARACTERS:
-            self.tvshow = self.tvshow.replace(illegal_character, REPLACE_CHAR)
+            self.title = self.title.replace(illegal_character, REPLACE_CHAR)
 
         # Replace the ellipsis with three periods to prevent UnicodeError
-        self.tvshow.replace("…",  "...")
+        self.title = self.title.replace("…",  "...")
 
 
 def process_files(directory):
@@ -78,19 +78,22 @@ def process_files(directory):
             if split[-1].upper() in FILE_TYPES:
                 tvshow = TVShow(os.extsep.join(split[0:-1]))
                 tvshow.process()
-                try: tvshow.get_id()
+                try:
+                    tvshow.get_id()
                 except AttributeError:
-                    print("* Error, skipping:", file_)
+                    print("Skipped:", file_)
                     continue
                 tvshow.get_episode()
                 tvshow.replace_illegal_characters()
-                r.rename(os.sep.join([directory, file_]), str(tvshow), extension=split[-1], extensions=FILE_TYPES)
-
+                r.rename(os.sep.join([directory, file_]), str(tvshow),
+                         extension=split[-1], extensions=FILE_TYPES)
 
         print("\nProcessing complete.")
-    else: raise IOError("Directory doesn't exist!")
+    else:
+        raise IOError("Directory doesn't exist!")
 
 if __name__ == "__main__":
     if len(sys.argv) == 2:
         process_files(sys.argv[1])
-    else: print("Missing target Directory")
+    else:
+        print("Missing target Directory")
